00488e8fd68837c570db5baedf88435add8a4430
CodeWithIftekhar <codewithiftekhar@gmail.com>
2023-10-20 14:36:41 +0600

    fix some deisgn of slier

--- src/pages/BrandPage/ProductCarousel.jsx
+++ src/pages/BrandPage/ProductCarousel.jsx
@@ -2,13 +2,13 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a lo
 import { Carousel } from "react-responsive-carousel";
 const ProductCarousel = ({ products }) => {
   return (
-    <Carousel slideInterval={2000}>
+    <Carousel slideInterval={2000} showThumbs={false}>
       {products
         ?.slice(0, 3)
         .map(({ _id, product_img, product_name, product_description }) => (
           <div className="" key={_id}>
             <img alt="..." src={product_img} className=" h-[500px]" />
-            <p className="legend flex flex-col ">
+            <p className="legend flex flex-col">
               <span className="text-3xl font-bold ">{product_name}</span>{" "}
               <span className="text-sm">{product_description.substring(0,300)}...</span>
             </p>
